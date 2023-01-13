61105f2e4e1d4d1d97ed2b54fe8f2a6d1814652c
Eduardo-M-Martins <martins-dudu@hotmail.com>
2023-01-13 12:00:06 -0300

    Settings class upgraded

--- src/main/java/poo/Settings.java
+++ src/main/java/poo/Settings.java
@@ -14,6 +14,9 @@ import javafx.scene.text.Font;
 import javafx.geometry.Pos;
 
 public class Settings extends Application {
+    public static final String RED_PATTERN = "-fx-text-fill: red; -fx-font-weight: bold; -fx-font-size: 22; -fx-font-family:Rockwell; -fx-background-color: transparent; -fx-border-color: transparent;";
+    public static final String GREEN_PATTERN = "-fx-text-fill: green; -fx-font-weight: bold; -fx-font-size: 22; -fx-font-family:Rockwell; -fx-background-color: transparent; -fx-border-color: transparent;";
+
     public static String DUMBAMOUNT = "8";
     public static String ZOMBIEAMOUNT = "5";
     public static String RUNNERAMOUNT = "4";
@@ -22,7 +25,8 @@ public class Settings extends Application {
 
     public void start(Stage stage) {
         BackgroundImage bgi = new BackgroundImage(new Image("file:Images/brown.jpg", 1800, 1000, false, true),
-                BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, BackgroundSize.DEFAULT);
+                BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER,
+                BackgroundSize.DEFAULT);
 
         Button back = new Button();
         back.setAlignment(Pos.BOTTOM_CENTER);
@@ -117,160 +121,82 @@ public class Settings extends Application {
             });
         });
 
-        TextField tx1 = new TextField();
-        tx1.setStyle("-fx-text-fill: white;");
-        tx1.setBorder(Border.EMPTY);
-        tx1.setBackground(Background.EMPTY);
-        tx1.setFont(Font.font("Rockwell", 16));
-        tx1.setMinWidth(230);
-        tx1.setEditable(false);
-        tx1.setAlignment(Pos.CENTER);
-        tx1.setText("Amount of Dumbs: ");
-
         inputText.get("dumbAmount").setText(DUMBAMOUNT);
+        inputText.get("healerAmount").setText(HEALERAMOUNT);
+        inputText.get("soldierAmount").setText(SOLDIERAMOUNT);
+        inputText.get("zombieAmount").setText(ZOMBIEAMOUNT);
+        inputText.get("runnerAmount").setText(RUNNERAMOUNT);
 
-        Button s2 = new Button("-50 pts");
-        s2.setBorder(Border.EMPTY);
-        s2.setBackground(Background.EMPTY);
-        s2.setFont(Font.font("Rockwell", 22));
-        s2.setStyle("-fx-text-fill: red; -fx-font-weight: bold;-fx-font-size: 24; -fx-font-family:Rockwell");
-        s2.setOnMouseEntered(e -> s2
-                .setStyle("-fx-text-fill: red; -fx-font-weight: bold;-fx-font-size: 24; -fx-font-family:Rockwell"));
-        s2.setOnMouseExited(e -> s2
-                .setStyle("-fx-text-fill: red; -fx-font-weight: bold; -fx-font-size: 24; -fx-font-family:Rockwell"));
+        HashMap<String, TextField> texFields = new HashMap();
+        texFields.put("Amount of Dumbs: ", new TextField());
+        texFields.put("Amount of Healers: ", new TextField());
+        texFields.put("Amount of Soldiers: ", new TextField());
+        texFields.put("Amount of Zombies: ", new TextField());
+        texFields.put("Amount of Runners: ", new TextField());
+
+        texFields.forEach((key, value) -> {
+            value.setStyle("-fx-text-fill: white; -fx-background-color: transparent; -fx-border-color: transparent; -fx-font-size: 16; -fx-font-family:Rockwell;");
+            value.setMinWidth(230);
+            value.setEditable(false);
+            value.setAlignment(Pos.CENTER);
+            value.setText(key);
+        });
+
+        Button dumbPts = new Button("-50 pts");
+        dumbPts.setStyle(RED_PATTERN);
+        Button healerPts = new Button("-75 pts");
+        healerPts.setStyle(RED_PATTERN);
+        Button soldierPts = new Button("-100 pts");
+        soldierPts.setStyle(RED_PATTERN);
+        Button zombiePts = new Button("+200 pts");
+        zombiePts.setStyle(GREEN_PATTERN);
+        Button runnerPts = new Button("+300 pts");
+        runnerPts.setStyle(GREEN_PATTERN);
 
         HBox h1 = new HBox();
         h1.setBackground(new Background(bgi));
         h1.setAlignment(Pos.CENTER_LEFT);
         h1.getChildren().add(s11);
         h1.getChildren().add(new ImageView(new Image("file:Images/dumb.jpeg", 120, 120, false, true)));
-        h1.getChildren().add(tx1);
+        h1.getChildren().add(texFields.get("Amount of Dumbs: "));
         h1.getChildren().add(inputText.get("dumbAmount"));
-        h1.getChildren().add(s2);
-
-        TextField tx2 = new TextField();
-        tx2.setStyle("-fx-text-fill: white;");
-        tx2.setBorder(Border.EMPTY);
-        tx2.setBackground(Background.EMPTY);
-        tx2.setFont(Font.font("Rockwell", 16));
-        tx2.setMinWidth(230);
-        tx2.setEditable(false);
-        tx2.setAlignment(Pos.CENTER);
-        tx2.setText("Amount of Healers: ");
-
-        inputText.get("healerAmount").setText(HEALERAMOUNT);
-
-        Button s3 = new Button("-75 pts");
-        s3.setBorder(Border.EMPTY);
-        s3.setBackground(Background.EMPTY);
-        s3.setFont(Font.font("Rockwell", 22));
-        s3.setStyle("-fx-text-fill: red; -fx-font-weight: bold;-fx-font-size: 24; -fx-font-family:Rockwell");
-        s3.setOnMouseEntered(e -> s3
-                .setStyle("-fx-text-fill: red; -fx-font-weight: bold;-fx-font-size: 24; -fx-font-family:Rockwell"));
-        s3.setOnMouseExited(e -> s3
-                .setStyle("-fx-text-fill: red; -fx-font-weight: bold; -fx-font-size: 24; -fx-font-family:Rockwell"));
+        h1.getChildren().add(dumbPts);
 
         HBox h2 = new HBox();
         h2.setBackground(new Background(bgi));
         h2.setAlignment(Pos.CENTER_LEFT);
         h2.getChildren().add(s21);
         h2.getChildren().add(new ImageView(new Image("file:Images/healer.jpeg", 120, 120, false, true)));
-        h2.getChildren().add(tx2);
+        h2.getChildren().add(texFields.get("Amount of Healers: "));
         h2.getChildren().add(inputText.get("healerAmount"));
-        h2.getChildren().add(s3);
-
-        TextField tx3 = new TextField();
-        tx3.setStyle("-fx-text-fill: white;");
-        tx3.setBorder(Border.EMPTY);
-        tx3.setBackground(Background.EMPTY);
-        tx3.setFont(Font.font("Rockwell", 16));
-        tx3.setMinWidth(230);
-        tx3.setEditable(false);
-        tx3.setAlignment(Pos.CENTER);
-        tx3.setText("Amount of Soldiers: ");
-
-        inputText.get("soldierAmount").setText(SOLDIERAMOUNT);
-
-        Button s4 = new Button("-100 pts");
-        s4.setBorder(Border.EMPTY);
-        s4.setBackground(Background.EMPTY);
-        s4.setFont(Font.font("Rockwell", 22));
-        s4.setStyle("-fx-text-fill: red; -fx-font-weight: bold;-fx-font-size: 24; -fx-font-family:Rockwell");
-        s4.setOnMouseEntered(e -> s4
-                .setStyle("-fx-text-fill: red; -fx-font-weight: bold;-fx-font-size: 24; -fx-font-family:Rockwell"));
-        s4.setOnMouseExited(e -> s4
-                .setStyle("-fx-text-fill: red; -fx-font-weight: bold; -fx-font-size: 24; -fx-font-family:Rockwell"));
+        h2.getChildren().add(healerPts);
 
         HBox h3 = new HBox();
         h3.setBackground(new Background(bgi));
         h3.setAlignment(Pos.CENTER_LEFT);
         h3.getChildren().add(s31);
         h3.getChildren().add(new ImageView(new Image("file:Images/soldier.jpeg", 120, 120, false, true)));
-        h3.getChildren().add(tx3);
+        h3.getChildren().add(texFields.get("Amount of Soldiers: "));
         h3.getChildren().add(inputText.get("soldierAmount"));
-        h3.getChildren().add(s4);
-
-        TextField tx4 = new TextField();
-        tx4.setStyle("-fx-text-fill: white;");
-        tx4.setBorder(Border.EMPTY);
-        tx4.setBackground(Background.EMPTY);
-        tx4.setFont(Font.font("Rockwell", 16));
-        tx4.setMinWidth(230);
-        tx4.setEditable(false);
-        tx4.setAlignment(Pos.CENTER);
-        tx4.setText("Amount of Zombies: ");
-
-        inputText.get("zombieAmount").setText(ZOMBIEAMOUNT);
-
-        Button s5 = new Button("+200 pts");
-        s5.setBorder(Border.EMPTY);
-        s5.setBackground(Background.EMPTY);
-        s5.setFont(Font.font("Rockwell", 22));
-        s5.setStyle("-fx-text-fill: green; -fx-font-weight: bold;-fx-font-size: 24; -fx-font-family:Rockwell");
-        s5.setOnMouseEntered(e -> s5
-                .setStyle("-fx-text-fill: green; -fx-font-weight: bold;-fx-font-size: 24; -fx-font-family:Rockwell"));
-        s5.setOnMouseExited(e -> s5
-                .setStyle("-fx-text-fill: green; -fx-font-weight: bold; -fx-font-size: 24; -fx-font-family:Rockwell"));
+        h3.getChildren().add(soldierPts);
 
         HBox h4 = new HBox();
         h4.setBackground(new Background(bgi));
         h4.setAlignment(Pos.CENTER_LEFT);
         h4.getChildren().add(s41);
         h4.getChildren().add(new ImageView(new Image("file:Images/zombie.jpeg", 120, 120, false, true)));
-        h4.getChildren().add(tx4);
+        h4.getChildren().add(texFields.get("Amount of Zombies: "));
         h4.getChildren().add(inputText.get("zombieAmount"));
-        h4.getChildren().add(s5);
-
-        TextField tx5 = new TextField();
-        tx5.setStyle("-fx-text-fill: white;");
-        tx5.setBorder(Border.EMPTY);
-        tx5.setBackground(Background.EMPTY);
-        tx5.setFont(Font.font("Rockwell", 16));
-        tx5.setMinWidth(230);
-        tx5.setEditable(false);
-        tx5.setAlignment(Pos.CENTER);
-        tx5.setText("Amount of Runners: ");
-
-        inputText.get("runnerAmount").setText(RUNNERAMOUNT);
-
-        Button s6 = new Button("+300 pts");
-        s6.setBorder(Border.EMPTY);
-        s6.setBackground(Background.EMPTY);
-        s6.setFont(Font.font("Rockwell", 22));
-        s6.setStyle("-fx-text-fill: green; -fx-font-weight: bold;-fx-font-size: 24; -fx-font-family:Rockwell");
-        s6.setOnMouseEntered(e -> s6
-                .setStyle("-fx-text-fill: green; -fx-font-weight: bold;-fx-font-size: 24; -fx-font-family:Rockwell"));
-        s6.setOnMouseExited(e -> s6
-                .setStyle("-fx-text-fill: green; -fx-font-weight: bold; -fx-font-size: 24; -fx-font-family:Rockwell"));
+        h4.getChildren().add(zombiePts);
 
         HBox h5 = new HBox();
         h5.setBackground(new Background(bgi));
         h5.setAlignment(Pos.CENTER_LEFT);
         h5.getChildren().add(s51);
         h5.getChildren().add(new ImageView(new Image("file:Images/runner.jpeg", 120, 120, false, true)));
-        h5.getChildren().add(tx5);
+        h5.getChildren().add(texFields.get("Amount of Runners: "));
         h5.getChildren().add(inputText.get("runnerAmount"));
-        h5.getChildren().add(s6);
+        h5.getChildren().add(runnerPts);
 
         b1.setOnAction((event) -> {
             inputText.get("dumbAmount").setText(inputText.get("dumbAmount").getText());
@@ -278,11 +204,11 @@ public class Settings extends Application {
             inputText.get("soldierAmount").setText(inputText.get("soldierAmount").getText());
             inputText.get("zombieAmount").setText(inputText.get("zombieAmount").getText());
             inputText.get("runnerAmount").setText(inputText.get("runnerAmount").getText());
-            setDumb(inputText.get("dumbAmount").getText());
-            setHealer(inputText.get("healerAmount").getText());
-            setSoldier(inputText.get("soldierAmount").getText());
-            setZombie(inputText.get("zombieAmount").getText());
-            setRunner(inputText.get("runnerAmount").getText());
+            this.DUMBAMOUNT = inputText.get("dumbAmount").getText();
+            this.HEALERAMOUNT = inputText.get("healerAmount").getText();
+            this.SOLDIERAMOUNT = inputText.get("soldierAmount").getText();
+            this.ZOMBIEAMOUNT = inputText.get("zombieAmount").getText();
+            this.RUNNERAMOUNT = inputText.get("runnerAmount").getText();
             createAmounts();
         });
 
@@ -329,22 +255,6 @@ public class Settings extends Application {
             }
             return n;
         }
-    }
-
-    public void setDumb(String n) {
-        this.DUMBAMOUNT = n;
-    }
-    public void setHealer(String n) {
-        this.HEALERAMOUNT = n;
-    }
-    public void setSoldier(String n) {
-        this.SOLDIERAMOUNT = n;
-    }
-    public void setZombie(String n) {
-        this.ZOMBIEAMOUNT = n;
-    }
-    public void setRunner(String n) {
-        this.RUNNERAMOUNT = n;
     }
 
     public boolean createAmounts() {
